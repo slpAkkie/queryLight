50770a8bf50e3ccb2cbac68a2a3633aef898cbaf
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-08-09 23:53:50 +0300

    Update gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,16 @@ const fs = require( 'fs' )
 
 const ts = require( 'gulp-typescript' )
 const uglifier = require( 'gulp-uglify' )
+const rename = require( 'gulp-rename' )
+const sourcemap = require( 'gulp-sourcemaps' )
 
 function compileTS() {
   return gulp.src( './src/queryLight.ts' )
-    .pipe( ts( {
-      target: 'es6'
-    } ) )
+    .pipe( sourcemap.init() )
+    .pipe( ts( { target: 'es6' } ) )
     .pipe( uglifier() )
+    .pipe( rename( { suffix: '.min' } ) )
+    .pipe( sourcemap.write( './' ) )
     .pipe( gulp.dest( './dist/' ) )
 }
 
